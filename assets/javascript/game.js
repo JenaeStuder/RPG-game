329324be5f61ae211fd42c5367767082a2cba7e8
Jenae <Jenaenay15@yahoo.com>
2018-11-25 22:54:04 -0700

    added html, css and JS elements

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+var kylee ={
+    startValue: 200
+}
+var mullet={
+    startValue: 150
+}
+var animals={
+    startValue: 150
+}
+var topless={
+    startValue: 150
+}
+
+$("#kylee").click(function(event){
+    
+})
+$("#mullet").click(function(event){
+    
+})
+$("#animals").click(function(event){
+    
+})
+$("#topless").click(function(event){
+    
+})
